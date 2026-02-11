fc42e34ed26d40feca9549be7a26551a34bf5669
BigDreac <ddeepikasharma85@gmail.com>
2026-02-11 19:10:47 +0530

    final

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 io.on("connection", (socket) => {
     socket.on("chat message", (msg) => {
