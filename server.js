c23ffca924be88920ad3dcbcc32dde086a62b28d
BigDreac <ddeepikasharma85@gmail.com>
2026-02-12 12:49:21 +0530

    J

--- server.js
+++ server.js
@@ -6,16 +6,38 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve index.html manually
+let activeUsers = 0;
+const MAX_USERS = 2;
+
+// Serve static files (if you later add CSS/JS files)
+app.use(express.static("public"));
+
+// Serve index.html
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
 io.on("connection", (socket) => {
 
+    // 🚫 Block if limit reached
+    if (activeUsers >= MAX_USERS) {
+        socket.emit("limitReached", "Server full. Only 2 users allowed at a time.");
+        socket.disconnect(true);
+        return;
+    }
+
+    activeUsers++;
+    console.log("User connected. Active users:", activeUsers);
+
+    // Send updates to others
     socket.on("update", (data) => {
-        // Send to everyone except sender
         socket.broadcast.emit("update", data);
+    });
+
+    // When user leaves
+    socket.on("disconnect", () => {
+        activeUsers--;
+        console.log("User disconnected. Active users:", activeUsers);
     });
 
 });
